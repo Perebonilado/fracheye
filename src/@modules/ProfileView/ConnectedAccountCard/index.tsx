94ce57cce534cf29fb31eab5db66f2e3ee2bff0e
perebonilado <reradiri@eminenttechnology.com>
2024-02-27 20:33:27 +0100

    connected account card

--- src/@modules/ProfileView/ConnectedAccountCard/index.tsx
+++ src/@modules/ProfileView/ConnectedAccountCard/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import Card from "../../../@shared-components/Card";
 import { SocialMediaPlatformModel } from "../../../models/SocialMediaPlatform.model";
 import InstagramIcon from "../../../@icons/InstagramIcon";
+import ArrowRight from "../../../@icons/ArrowRight";
 
 interface Props {
   totalLifeTimeViewsCount: number;
@@ -20,11 +21,36 @@ const ConnectedAccountCard: FC<Props> = ({
 }) => {
   const socialMediaPlatformIcons = {
     instagram: <InstagramIcon />,
+    twitter: <InstagramIcon />,
+    facebook: <InstagramIcon />,
   } as const;
+
+  const stats = {
+    "Total lifetime views": totalLifeTimeViewsCount,
+    Likes: likeCount,
+    Followers: followerCount,
+  };
 
   return (
     <Card>
-      <div className="flex items-center justify-between"></div>
+      <div className="flex items-center justify-between">
+        {socialMediaPlatformIcons[socialMediaPlatform]}
+
+        <p className="text-base font-medium">{username}</p>
+
+        <ArrowRight />
+      </div>
+
+      <div>
+        {Object.entries(stats).map(([title, count]) => {
+          return (
+            <div className="flex flex-col gap-4 text-center">
+              <p className="text-sm text-[#000000B2]">{title}</p>
+              <p className="text-xs text-[#00000066]">{count}</p>
+            </div>
+          );
+        })}
+      </div>
     </Card>
   );
 };
